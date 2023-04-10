6172d4851d440fab2c6273681602726a1a82ef25
SuphachokeKKU <130337039+SuphachokeKKU@users.noreply.github.com>
2023-04-10 13:14:32 +0700

    Update test.py

--- test.py
+++ test.py
@@ -2,9 +2,16 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import mysql.connector
-import requests
 
 st.title("Test Streamlit deploy!!!")
 
-r = requests.get("https://wunchana.mdsolutions.in.th")
-st.text(r.text)
+conn = mysql.connector.connect(host=st.secrets['db_host'],user=st.secrets['db_user'],password=st.secrets['db_pass'])
+
+cur = None
+if(conn.is_connected()):
+    st.text("Connect to DBMS succesful!")
+    cur = conn.cursor()
+    cur.execute("show databases;")
+    st.text(cur.fetchall())
+else:
+    st.text("Connect to DBMS error!")
