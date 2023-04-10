7b6b889b7970f104552ea34040b85602c38d08de
SuphachokeKKU <130337039+SuphachokeKKU@users.noreply.github.com>
2023-04-10 12:49:43 +0700

    Update test.py

--- test.py
+++ test.py
@@ -5,4 +5,13 @@ import mysql.connector
 
 st.title("Test Streamlit deploy!!!")
 
-st.text(st.secrets['db_host'])
+conn = mysql.connector.connect(host=st.secrets['db_host'],user=st.secrets['db_user'],password=st.secrets['db_pass'])
+
+cur = None
+if(conn.is_connected()):
+    st.text("Connect to DBMS succesful!")
+    cur = conn.cursor()
+    cur.execute("show databases;")
+    st.text(cur.fetchall())
+else:
+    st.text("Connect to DBMS error!")
