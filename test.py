b52ad1131d5d9d3b347f3721c18cd39600185999
SuphachokeKKU <130337039+SuphachokeKKU@users.noreply.github.com>
2023-04-27 23:47:50 +0700

    Update test.py

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ import mysql.connector
 
 st.title("Test Streamlit deploy!!!")
 
-conn = mysql.connector.connect(host=st.secrets['db_host'],user=st.secrets['db_user'],password=st.secrets['db_pass'])
+conn = mysql.connector.connect(host=st.secrets['db_host'], port=st.secrets['db_port'], user=st.secrets['db_user'],password=st.secrets['db_pass'])
 
 cur = None
 if(conn.is_connected()):
